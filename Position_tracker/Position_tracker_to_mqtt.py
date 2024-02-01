d1769162718e9017ee98619e1a569359296f08a9
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-02-01 15:47:05 -0300

    .

--- Position_tracker/Position_tracker_to_mqtt.py
+++ Position_tracker/Position_tracker_to_mqtt.py
@@ -9,29 +9,34 @@ from paho import mqtt
 import pyttsx3
 
 def on_connect(client, userdata, flags, rc, properties=None):
-    #print("CONNACK received with code %s." % rc)
-    # Subscribe to the "idle_rx" topic when connected
     client.subscribe("idle_rx", qos=1)
+
 def on_publish(client, userdata, mid, properties=None):
     print("publicado")
-    #print("mid: " + str(mid))
+
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
     print("Subscrito")
-    #print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 def on_message(client, userdata, msg):
     mensagem = msg.payload.decode()
     print("-----------")
     print(f"Mensagem recebida: {mensagem}")
     print("-----------")
-    # Handle the received message here
-    #display_countdown(remaining_seconds, msg.payload.decode())
+
+def on_disconnect(client, userdata, rc, properties=None):
+    print(f"Disconnected with result code {rc}. Reconnecting...")
+    client.reconnect()
 
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
+client.on_disconnect = on_disconnect
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 client.username_pw_set("position_tracker", "Digital1")
-client.connect("dd6e8d1cc8524360a537e7db4e5924f8.s2.eu.hivemq.cloud", 8883)
-topico = "garra-POS" 
+client.connect("dd6e8d1cc8524360a537e7db4e5924f8.s2.eu.hivemq.cloud", 8883, keepalive=5)  # Include keepalive parameter
+topico = "garraxyzr"
+client.publish(topico, "vai filhao", 1)
+
+stop_observer = False  # Flag to signal observer to stop and join
 
 
 class MyHandler(FileSystemEventHandler):
@@ -40,15 +45,22 @@ class MyHandler(FileSystemEventHandler):
         self.pacote = None
 
     def on_created(self, event):
+      
         if event.is_directory:
             return
         elif event.src_path.endswith(".txt"):
             print(f"Novo arquivo criado: {event.src_path}")
-            str_pub = event.src_path.replace("C:\\Users\\João Fernando Rangel\\Desktop\\Digital Twin\\DEMO_DT\\Position_tracker\\Logs\\log___","")
+           #str_pub = event.src_path.replace("C:\\Users\\João Fernando Rangel\\Desktop\\Digital Twin\\DEMO_DT\\Position_tracker\\Logs\\log___","")
+            str_pub = event.src_path.replace("C:\\Users\\Digital Twin\\Documents\\GitHub\\DEMO_DT\\Position_tracker\\Logs\\log___", "")
+            print('.')
             str_pub = str_pub.replace(".txt", "")
-            str_pub = str_pub.replace("-", ":")
+            
+            print('.......')
             client.publish(topico, str_pub, 1)
+            client.loop(1,10)
+            print('----------')
             self.pacote = event.src_path
+            
 
 def delete_files_with_prefix(folder, prefix):
     for file_name in os.listdir(folder):
@@ -61,29 +73,32 @@ def delete_files_with_prefix(folder, prefix):
                 print(f"Erro ao remover o arquivo {file_path}: {e}")
 
 def on_press(key):
+    global stop_observer
     if key == keyboard.Key.esc:
         print("Tecla Esc pressionada. Removendo arquivos e encerrando o programa.")
         delete_files_with_prefix(pasta_vigilancia, "log___")
         client.publish(topico, "Movimentação terminada", 1)
-        observer.stop()
+        stop_observer = True
 
 def vigiar_pasta(pasta):
     event_handler = MyHandler(pasta)
     global observer
-    observer = Observer() 
+    observer = Observer()
     observer.schedule(event_handler, pasta, recursive=False)
     observer.start()
 
     with keyboard.Listener(on_press=on_press) as listener:
         try:
-            while True:
+            while not stop_observer:
                 time.sleep(1)
         except KeyboardInterrupt:
+            pass
+        finally:
             observer.stop()
-        observer.join()
+            observer.join()
 
 if __name__ == "__main__":
-    #pasta_vigilancia = "C:\\Users\\Digital Twin\\Documents\\GitHub\\DEMO_DT\\Position_tracker\\Logs"
-    pasta_vigilancia = "C:\\Users\\João Fernando Rangel\\Desktop\\Digital Twin\\DEMO_DT\\Position_tracker\\Logs"
+    pasta_vigilancia = "C:\\Users\\Digital Twin\\Documents\\GitHub\\DEMO_DT\\Position_tracker\\Logs"
+    #pasta_vigilancia = "C:\\Users\\João Fernando Rangel\\Desktop\\Digital Twin\\DEMO_DT\\Position_tracker\\Logs"
+    
     vigiar_pasta(pasta_vigilancia)
-
